482051fac3cc0cfdbf8d4c046e82f33d4fa5f249
choudharymac <macdevil28@gmail.com>
2017-05-30 18:22:27 +0530

    Login+Signup Module
    Login+Signup Module with Amazon Cognito

--- app/src/main/java/sit374_team17/propertyinspector/Activity_Create.java
+++ app/src/main/java/sit374_team17/propertyinspector/Activity_Create.java
@@ -3,16 +3,50 @@ package sit374_team17.propertyinspector;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
-public class Activity_Create extends AppCompatActivity {
+import com.amazonaws.ClientConfiguration;
+import com.amazonaws.mobileconnectors.cognitoidentityprovider.CognitoUser;
+import com.amazonaws.mobileconnectors.cognitoidentityprovider.CognitoUserAttributes;
+import com.amazonaws.mobileconnectors.cognitoidentityprovider.CognitoUserCodeDeliveryDetails;
+import com.amazonaws.mobileconnectors.cognitoidentityprovider.CognitoUserPool;
+import com.amazonaws.mobileconnectors.cognitoidentityprovider.handlers.SignUpHandler;
+import com.amazonaws.regions.Regions;
+
+public class Activity_Create extends AppCompatActivity implements SignUpHandler{
 
     Spinner date_of_birth,month_of_birth,year_of_birth;
     ArrayAdapter dateAdapter,monthAdapter,yearAdapter;
     String [] date_range=new String[31];
     String [] year_range=new String[100];
     int tempYear=1917;
+    Button submitToSignUp ;
+    EditText edt_firstName,edt_surName,edt_email,edt_password,edt_mobile;
+    private static String LOG_TAG="PropertyInspector";
+    protected CognitoUserPool userPool;
+    /**
+     * Cognito Your Identity Pool ID
+     */
+    protected final String userPoolId="ap-southeast-2_e4nCxiblG";
+    /**
+     * Client ID created for your pool {@code userPoolId}.
+     */
+    protected final String clientId="7urpmpfrej7qj5msrpju7hagum";
+
+    /**
+     * Client secret generated for this {@code clientId}, this may be {@code null} if a secret is not
+     * generated for the {@code clientId}.
+     */
+    protected final String clientSecret="1vtjn8qulev7dg5c51jb0kq31jo27h208uqa9grj1agmr8uvckd7";
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,20 +56,58 @@ public class Activity_Create extends AppCompatActivity {
         setSupportActionBar(toolbar);
         //Hides the Toolbar in this activity
         getSupportActionBar().hide();
-
         initViews();
     }
 
+    //View are initialize and operated below
     void initViews()
     {
         date_of_birth=(Spinner)findViewById(R.id.date_of_birth);
         month_of_birth=(Spinner)findViewById(R.id.month_of_birth);
         year_of_birth=(Spinner)findViewById(R.id.year_of_birth);
 
+        edt_firstName=(EditText)findViewById(R.id.edt_firstName);
+        edt_surName=(EditText)findViewById(R.id.edt_surName);
+        edt_email=(EditText)findViewById(R.id.edt_email);
+        edt_password=(EditText)findViewById(R.id.edt_password);
+        edt_mobile=(EditText)findViewById(R.id.edt_mobile);
+
+        submitToSignUp=(Button)findViewById(R.id.submitToSignUp);
+        submitToSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (checkEmpty(edt_firstName)||checkEmpty(edt_surName)||checkEmpty(edt_email)||checkEmpty(edt_password)||checkEmpty(edt_mobile))
+                    Toast.makeText(getApplicationContext(),"Fields cannot be empty ",Toast.LENGTH_LONG).show();
+                else{
+                    Toast.makeText(getApplicationContext(),"Processing your request.. ",Toast.LENGTH_LONG).show();
+                    ClientConfiguration clientConfiguration = new ClientConfiguration();
+                    /**
+                     * This represents a user-pool in a Cognito identity provider account.
+                     */
+                    userPool = new CognitoUserPool(Activity_Create.this,userPoolId ,clientId ,clientSecret, clientConfiguration, Regions.AP_SOUTHEAST_2);
+                    // Create a CognitoUserAttributes object and add user attributes
+                    CognitoUserAttributes userAttributes = new CognitoUserAttributes();
+                    // Add the user attributes. Attributes are added as key-value pairs
+                    // Adding user's given name.
+                    // Note that the key is "given_name" which is the OIDC claim for given name
+                    userAttributes.addAttribute("given_name", edt_firstName.getText().toString().concat(" ").concat(edt_surName.getText().toString()));
+                    // Adding user's phone number
+                    userAttributes.addAttribute("phone_number", edt_mobile.getText().toString());
+                    // Adding user's email address
+                    userAttributes.addAttribute("email", edt_email.getText().toString());
+                    // Adding user's family_name
+                    userAttributes.addAttribute("family_name", edt_firstName.getText().toString().concat(" ").concat(edt_surName.getText().toString()));
+                    // Adding user's address
+                    userAttributes.addAttribute("address", "None");
+
+                    userPool.signUpInBackground( edt_email.getText().toString(),edt_password.getText().toString(), userAttributes, null, Activity_Create.this);
+                }
+            }
+        });
         addValues();
     }
 
-    void addValues()
+    protected void addValues()
     {
         //Add int values up to 31
         for (int i=0;i<31;i++)
@@ -59,4 +131,35 @@ public class Activity_Create extends AppCompatActivity {
         year_of_birth.setAdapter(yearAdapter);
     }
 
+    protected static boolean checkEmpty(EditText editText)
+    {
+        return editText.getText().toString().equals("");
+    }
+
+    /**
+     * Implemented methods of SignUpHandler
+     */
+    @Override
+    public void onSuccess(CognitoUser cognitoUser, boolean b, CognitoUserCodeDeliveryDetails cognitoUserCodeDeliveryDetails) {
+        // Sign-up was successful
+        Log.d(LOG_TAG,"Sign-up was successful");
+        Toast.makeText(getApplicationContext(),"Sign-up successfully and confirmation code was sent to your mail",Toast.LENGTH_SHORT).show();
+        // Check if this user (cognitoUser) needs to be confirmed
+        if(!b) {
+            // This user must be confirmed and a confirmation code was sent to the user
+            // cognitoUserCodeDeliveryDetails will indicate where the confirmation code was sent
+            // Get the confirmation code from user
+            Log.d(LOG_TAG,"This user must be confirmed and a confirmation code was sent to the user");
+        }
+        else {
+            // The user has already been confirmed
+            Log.d(LOG_TAG,"The user has already been confirmed");
+        }
+        finish();
+    }
+
+    @Override
+    public void onFailure(Exception e) {
+        Log.e(LOG_TAG,e.getMessage());
+    }
 }
--- app/src/main/java/sit374_team17/propertyinspector/Activity_Login.java
+++ app/src/main/java/sit374_team17/propertyinspector/Activity_Login.java
@@ -3,40 +3,169 @@ package sit374_team17.propertyinspector;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class Activity_Login extends AppCompatActivity {
+import com.amazonaws.ClientConfiguration;
+import com.amazonaws.mobileconnectors.cognitoidentityprovider.CognitoDevice;
+import com.amazonaws.mobileconnectors.cognitoidentityprovider.CognitoUser;
+import com.amazonaws.mobileconnectors.cognitoidentityprovider.CognitoUserDetails;
+import com.amazonaws.mobileconnectors.cognitoidentityprovider.CognitoUserPool;
+import com.amazonaws.mobileconnectors.cognitoidentityprovider.CognitoUserSession;
+import com.amazonaws.mobileconnectors.cognitoidentityprovider.continuations.AuthenticationContinuation;
+import com.amazonaws.mobileconnectors.cognitoidentityprovider.continuations.AuthenticationDetails;
+import com.amazonaws.mobileconnectors.cognitoidentityprovider.continuations.ChallengeContinuation;
+import com.amazonaws.mobileconnectors.cognitoidentityprovider.continuations.MultiFactorAuthenticationContinuation;
+import com.amazonaws.mobileconnectors.cognitoidentityprovider.continuations.NewPasswordContinuation;
+import com.amazonaws.mobileconnectors.cognitoidentityprovider.handlers.AuthenticationHandler;
+import com.amazonaws.mobileconnectors.cognitoidentityprovider.handlers.GetDetailsHandler;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.cognitoidentityprovider.model.NotAuthorizedException;
+import com.amazonaws.services.cognitoidentityprovider.model.UserNotConfirmedException;
+
+import java.util.Map;
+
+public class Activity_Login extends AppCompatActivity implements AuthenticationHandler {
+
+    protected CognitoUser cognitoUser;
+    protected CognitoUserPool userPool;
+    protected static String LOG_TAG="PropertyInspector";
+    private String idToken="";
+    private EditText edt_email,edt_password;
+    /**
+     * Cognito Your Identity Pool ID
+     */
+    protected final String userPoolId="ap-southeast-2_e4nCxiblG";
+    /**
+     * Client ID created for your pool {@code userPoolId}.
+     */
+    protected final String clientId="7urpmpfrej7qj5msrpju7hagum";
+
+    /**
+     * Client secret generated for this {@code clientId}, this may be {@code null} if a secret is not
+     * generated for the {@code clientId}.
+     */
+    protected final String clientSecret="1vtjn8qulev7dg5c51jb0kq31jo27h208uqa9grj1agmr8uvckd7";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
+        edt_email=(EditText) findViewById(R.id.editText_username);
+        edt_password=(EditText) findViewById(R.id.editText_password);
         Button button_goToLoginActivity = (Button) findViewById(R.id.button_goToMainActivity);
-        button_goToLoginActivity.setOnClickListener(new View.OnClickListener(){
+        button_goToLoginActivity.setOnClickListener(new View.OnClickListener() {
 
             @Override
-            public void onClick(View view){
-                Intent intent = new Intent(Activity_Login.this, MainActivity.class);
-                startActivity(intent);
+            public void onClick(View view) {
+                if (checkEmpty(edt_email)||checkEmpty(edt_email)) {
+                    Toast.makeText(getApplicationContext(),"Fields cannot be empty ",Toast.LENGTH_LONG).show();
+                }else
+                {
+                    ClientConfiguration clientConfiguration = new ClientConfiguration();
+                    // Create a CognitoUserPool object to refer to your user pool
+                    userPool = new CognitoUserPool(Activity_Login.this, userPoolId, clientId, clientSecret, clientConfiguration, Regions.AP_SOUTHEAST_2);
+                    // Create a CognitoUserPool object to refer to your user pool
+                    cognitoUser = userPool.getUser();
+                    cognitoUser.getSessionInBackground(Activity_Login.this);
+                }
             }
 
         });
-
         //Instance of the "Create Account" labeled TextView created and on click method opens the Activity_Create activity
         TextView text_goToCreateActivity = (TextView) findViewById(R.id.text_goToCreateActivity);
-        text_goToCreateActivity.setOnClickListener(new View.OnClickListener(){
+        text_goToCreateActivity.setOnClickListener(new View.OnClickListener() {
 
             @Override
-            public void onClick(View view){
+            public void onClick(View view) {
                 Intent intent = new Intent(Activity_Login.this, Activity_Create.class);
                 startActivity(intent);
             }
 
         });
+    }
+
+    // Implement callback handler for getting details
+    GetDetailsHandler getDetailsHandler = new GetDetailsHandler() {
+        @Override
+        public void onSuccess(CognitoUserDetails cognitoUserDetails) {
+            // The user detail are in cognitoUserDetails
+            Map mDetails=cognitoUserDetails.getAttributes().getAttributes();
+            Log.d(LOG_TAG,"Phone Number"+mDetails.get("phone_number"));
+            Log.d(LOG_TAG,"Email "+mDetails.get("email"));
+            Log.d(LOG_TAG,"Family Name"+mDetails.get("family_name"));
+            Intent intent = new Intent(Activity_Login.this, MainActivity.class);
+            intent.putExtra("tokens",idToken);
+            startActivity(intent);
+        }
+
+        @Override
+        public void onFailure(Exception exception) {
+            // Fetch user details failed, check exception for the cause
+            Log.e(LOG_TAG,"Failed"+exception.toString());
+        }
+    };
+
+    @Override
+    public void onSuccess(CognitoUserSession cognitoUserSession, CognitoDevice cognitoDevice) {
+        // Get id token from CognitoUserSession.
+        idToken = cognitoUserSession.getIdToken().getJWTToken();
+        // Fetch the user details
+        cognitoUser.getDetailsInBackground(getDetailsHandler);
 
     }
 
+    @Override
+    public void getAuthenticationDetails(AuthenticationContinuation authenticationContinuation, String s) {
+        Log.d(LOG_TAG,"Authentication is required and submitted successfully");
+        Toast.makeText(getApplicationContext(),"Checking credentials please wait..  ",Toast.LENGTH_LONG).show();
+        AuthenticationDetails authenticationDetails = new AuthenticationDetails(edt_email.getText().toString(), edt_password.getText().toString(), null);
+        //  AuthenticationDetails authenticationDetails = new AuthenticationDetails("John", "JohnSmith123", null);
+        // Pass the user sign-in credentials to the continuation
+        authenticationContinuation.setAuthenticationDetails(authenticationDetails);
+        // Allow the sign-in to continue
+        authenticationContinuation.continueTask();
+    }
+
+    @Override
+    public void getMFACode(MultiFactorAuthenticationContinuation multiFactorAuthenticationContinuation) {
+        // Multi-factor authentication is required; get the verification code from user;
+        //currently disable in this case
+    }
+
+    @Override
+    public void authenticationChallenge(ChallengeContinuation challengeContinuation) {
+        if (challengeContinuation.getParameters().get("requiredAttributes").length()>2)
+        {
+            challengeContinuation.setChallengeResponse("userAttributes.family_name", "None");
+            challengeContinuation.setChallengeResponse("userAttributes.given_name", "None");
+            challengeContinuation.setChallengeResponse("userAttributes.address", "None");
+        }
+        if ("NEW_PASSWORD_REQUIRED".equals(challengeContinuation.getChallengeName())) {
+            NewPasswordContinuation newPasswordContinuation = (NewPasswordContinuation) challengeContinuation;
+            newPasswordContinuation.setPassword( edt_password.getText().toString());
+            challengeContinuation.continueTask();
+        }
+
+    }
+
+    @Override
+    public void onFailure(Exception e) {
+        if (e instanceof UserNotConfirmedException)
+            Toast.makeText(getApplicationContext(),"User is not confirmed.Please confirm your account to proceed.",Toast.LENGTH_LONG).show();
+        else if (e instanceof NotAuthorizedException)
+            Toast.makeText(getApplicationContext(),"Either username or password is wrong ",Toast.LENGTH_LONG).show();
+
+    }
+    protected static boolean checkEmpty(EditText editText)
+    {
+        return editText.getText().toString().equals("");
+    }
+
+
 }
+
